22acd6720a6bf3d45f668b06c16b5cb8d2918fd4
Steve Hollasch <steve@hollasch.net>
2019-12-14 02:06:03 -0800

    restOfYourLife/hittable_list: convert to vector

--- src/TheRestOfYourLife/hittable_list.h
+++ src/TheRestOfYourLife/hittable_list.h
@@ -13,48 +13,48 @@
 
 #include "common/rtweekend.h"
 #include "hittable.h"
+#include <vector>
 
 
 class hittable_list: public hittable  {
     public:
         hittable_list() {}
-        hittable_list(hittable **l, int n) {list = l; list_size = n; }
-        virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const;
-        virtual bool bounding_box(double t0, double t1, aabb& output_box) const;
-        virtual double pdf_value(const vec3& o, const vec3& v) const;
-        virtual vec3 random(const vec3& o) const;
 
-        hittable **list;
-        int list_size;
+        void clear();
+        void add(hittable *object);
+
+        virtual bool bounding_box(double t0, double t1, aabb& output_box) const;
+        virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const;
+        virtual double pdf_value(const vec3 &o, const vec3 &v) const;
+        virtual vec3 random(const vec3 &o) const;
+
+        std::vector<hittable*> objects;
 };
 
-double hittable_list::pdf_value(const vec3& o, const vec3& v) const {
-    auto weight = 1.0/list_size;
-    auto sum = 0.0;
-    for (int i = 0; i < list_size; i++)
-        sum += weight*list[i]->pdf_value(o, v);
-    return sum;
+
+void hittable_list::clear() {
+    objects.clear();
 }
 
-vec3 hittable_list::random(const vec3& o) const {
-    int index = random_int(0, list_size-1);
-    return list[ index ]->random(o);
+
+void hittable_list::add(hittable *object) {
+    objects.push_back(object);
 }
 
 
 bool hittable_list::bounding_box(double t0, double t1, aabb& output_box) const {
-    if (list_size < 1) return false;
+    if (objects.empty()) return false;
 
     aabb temp_box;
-    bool first_true = list[0]->bounding_box(t0, t1, temp_box);
+    bool first_true = objects[0]->bounding_box(t0, t1, temp_box);
 
     if (!first_true)
         return false;
     else
         output_box = temp_box;
 
-    for (int i = 1; i < list_size; i++) {
-        if (list[i]->bounding_box(t0, t1, temp_box))
+    for (hittable* object : objects) {
+        if (object->bounding_box(t0, t1, temp_box))
             output_box = surrounding_box(output_box, temp_box);
         else
             return false;
@@ -63,13 +63,14 @@ bool hittable_list::bounding_box(double t0, double t1, aabb& output_box) const {
     return true;
 }
 
+
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     hit_record temp_rec;
     bool hit_anything = false;
     double closest_so_far = t_max;
 
-    for (int i = 0; i < list_size; i++) {
-        if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+    for (hittable *object : objects) {
+        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
             hit_anything = true;
             closest_so_far = temp_rec.t;
             rec = temp_rec;
@@ -77,5 +78,24 @@ bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& re
     }
     return hit_anything;
 }
+
+
+double hittable_list::pdf_value(const vec3 &o, const vec3 &v) const {
+    auto weight = 1.0/objects.size();
+    auto sum = 0.0;
+
+    for (auto object : objects)
+        sum += weight * object->pdf_value(o, v);
+
+    return sum;
+}
+
+
+vec3 hittable_list::random(const vec3 &o) const {
+    auto int_size = static_cast<int>(objects.size());
+    auto index = static_cast<size_t>(random_int(0, int_size-1));
+    return objects[index]->random(o);
+}
+
 
 #endif
