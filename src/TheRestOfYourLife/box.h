8607bd4d800d3e18a83e11f8bf417cabf69d4dce
Steve Hollasch <steve@hollasch.net>
2019-12-14 02:08:23 -0800

    Convert restLife/box.h to hittable vector

--- src/TheRestOfYourLife/box.h
+++ src/TheRestOfYourLife/box.h
@@ -36,14 +36,15 @@ class box: public hittable  {
 box::box(const vec3& p0, const vec3& p1, material *ptr) {
     pmin = p0;
     pmax = p1;
-    hittable **list = new hittable*[6];
-    list[0] = new xy_rect(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr);
-    list[1] = new flip_normals(new xy_rect(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr));
-    list[2] = new xz_rect(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr);
-    list[3] = new flip_normals(new xz_rect(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), ptr));
-    list[4] = new yz_rect(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr);
-    list[5] = new flip_normals(new yz_rect(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr));
-    list_ptr = new hittable_list(list,6);
+    hittable_list *sides = new hittable_list();
+    sides->add(new xy_rect(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr));
+    sides->add(new flip_normals(new xy_rect(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr)));
+    sides->add(new xz_rect(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr));
+    sides->add(new flip_normals(new xz_rect(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), ptr)));
+    sides->add(new yz_rect(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr));
+    sides->add(new flip_normals(new yz_rect(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr)));
+
+    list_ptr = sides;
 }
 
 bool box::hit(const ray& r, double t0, double t1, hit_record& rec) const {
