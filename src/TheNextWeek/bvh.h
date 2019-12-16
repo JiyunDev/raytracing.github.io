f383f588e3b7b63e863b1357ec7d29ffeb6bf7e7
Steve Hollasch <steve@hollasch.net>
2019-12-16 00:09:47 -0800

    theNextWeek/bvh.h: Use common box compare function

--- src/TheNextWeek/bvh.h
+++ src/TheNextWeek/bvh.h
@@ -37,36 +37,20 @@ class bvh_node : public hittable  {
 };
 
 
-bool box_x_compare (const hittable *a, const hittable *b) {
-    aabb box_a, box_b;
+bool box_compare(const hittable *a, const hittable *b, int axis) {
+    aabb box_a;
+    aabb box_b;
 
     if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b))
         std::cerr << "No bounding box in bvh_node constructor.\n";
 
-    return box_a.min().x() < box_b.min().x();
+    return box_a.min().e[axis] < box_b.min().e[axis];
 }
 
 
-bool box_y_compare (const hittable *a, const hittable *b)
-{
-    aabb box_a, box_b;
-
-    if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b))
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-
-    return box_a.min().y() < box_b.min().y();
-}
-
-
-bool box_z_compare (const hittable *a, const hittable *b)
-{
-    aabb box_a, box_b;
-
-    if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b))
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-
-    return box_a.min().z() < box_b.min().z();
-}
+bool box_x_compare (const hittable *a, const hittable *b) { return box_compare(a, b, 0); }
+bool box_y_compare (const hittable *a, const hittable *b) { return box_compare(a, b, 1); }
+bool box_z_compare (const hittable *a, const hittable *b) { return box_compare(a, b, 2); }
 
 
 bvh_node::bvh_node(
